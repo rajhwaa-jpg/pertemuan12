7dfc0b88e7a381712f4e6dee5adcef2599429fde
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-19 19:15:44 +0700

    menambahkan variable umur

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 class orang {
+public:
+    int Umur;
 
+    orang (int pUmur);
+    Umur(pUmur)
+    {
+        cout  << "orang dibuat dengan umur" << Umur << "\n" << endl;
+    }
 };
