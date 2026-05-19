87abb5c5e40811e56654edfb44cfe4186da25cbc
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-19 15:37:08 +0700

    feat: add 'remoteLampu' class with getter and setter for 'SaklarNo' in enkapsulasi.cpp

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class remoteLampu{
+private:
+    string SaklarNo[10];
+public:
+    void setSaKlarNo(int i, string value){
+        SaklarNo[i] = value;
+    }
+    string getSaKlarNo(int i) {
+        return SaklarNo[i];
+    }
+};
